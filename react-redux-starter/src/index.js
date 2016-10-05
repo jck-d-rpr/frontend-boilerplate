97d073663df343be09a66eb50a92840dadc1a0f2
Sherub Thakur <sherub.thakur@gmail.com>
2016-10-06 03:37:23 +0530

    removed unecessary projects

--- react-redux-starter/src/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-process-env */
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./utils/Index.prod');
-} else {
-  module.exports = require('./utils/Index.dev');
-}
--- react-starter/src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-
-import {} from 'bootstrap-webpack';
-
-ReactDOM.render(<App />, document.getElementById('placeholder'));
